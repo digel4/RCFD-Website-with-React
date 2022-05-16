eb012932c7b885acef26ef7f40eeb843e56944fb
digel4 <ofoxavondale@gmail.com>
2022-05-16 11:47:49 +0100

    remove react-router-dom req in server

--- client/src/APIS/events.js
+++ client/src/APIS/events.js
@@ -1,5 +1,12 @@
 import axios from 'axios';
 
+let baseURL
+if (process.env.NODE_ENV !== "production") {
+    baseURL= 'https://rcfd-react.herokuapp.com/'
+} else {
+    baseURL= 'http://localhost:3001'
+}
+
 export default axios.create({
-    baseURL: 'https://rcfd-react.herokuapp.com/'
+    baseURL
 })
